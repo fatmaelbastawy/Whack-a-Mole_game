c408b775d10f9c4c829ae4d1ede6d6440a5b5c68
fatma <fatmaelbastawy@gmail.com>
2024-10-02 14:05:01 -0700

    1st game

---
+++ script.js
@@ -0,0 +1,79 @@
+let currMoleTile;
+let currPlantTile;
+let score = 0;
+let gameOver = false;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    
+    for (let i = 0; i < 9; i++) { 
+
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        tile.addEventListener("click", selectTile);
+        document.getElementById("board").appendChild(tile);
+    }
+    setInterval(setMole, 1000); 
+    setInterval(setPlant, 1500); 
+}
+
+function getRandomTile() {
+   
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+}
+
+function setMole() {
+    if (gameOver) {
+        return;
+    }
+    if (currMoleTile) {
+        currMoleTile.innerHTML = "";
+    }
+    let mole = document.createElement("img");
+    mole.src = "./Images/monty-mole.png";
+
+    let num = getRandomTile();
+    if (currPlantTile && currPlantTile.id == num) {
+        return;
+    }
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+    if (gameOver) {
+        return;
+    }
+    if (currPlantTile) {
+        currPlantTile.innerHTML = "";
+    }
+    let plant = document.createElement("img");
+    plant.src = "./Images/piranha-plant.png";
+
+    let num = getRandomTile();
+    if (currMoleTile && currMoleTile.id == num) {
+        return;
+    }
+    currPlantTile = document.getElementById(num);
+    currPlantTile.appendChild(plant);
+}
+
+function selectTile() {
+    if (gameOver) {
+        return;
+    }
+    if (this == currMoleTile) {
+        score += 10;
+        document.getElementById("score").innerText = score.toString();
+        console.log("Hit mole! Score: " + score);
+    }
+    else if (this == currPlantTile) {
+        document.getElementById("score").innerText = "GAME OVER ! "+ score; 
+        console.log("Game Over! Final score: " + score);
+        gameOver = true;
+    }
+}
